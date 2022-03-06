37af1e0ff032eb367a8645ac6a1669d577f30559
regexart <regexart@gmail.com>
2022-03-06 21:31:12 +0300

    MomentJS kaynaklı bir hata sebebiyle oluşturulma zamanı özelliği geçici olarak kaldırıldı

--- src/components/Subheader/index.js
+++ src/components/Subheader/index.js
@@ -1,20 +1,14 @@
-import { useRouter } from 'next/router'
 import { settings } from '../../data/settings'
-import moment from 'moment/min/moment-with-locales'
 import QRCode from 'qrcode.react'
+import moment from 'moment'
 
 const Subheader = ({ data }) => {
-  const router = useRouter()
-  moment.locale(router.locale)
-
   return (
     <div className="subheader">
       <div className="subheader-details">
         <div className="subheader-short">{data.short}</div>
         <div className="subheader-date">
           {moment(data.createdAt).format('DD.MM.YYYY HH:mm:ss')}
-          {` - `}
-          {moment(data.createdAt).fromNow()}
         </div>
       </div>
       <div className="subheader-qrcode">
