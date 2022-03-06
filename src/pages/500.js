709e0c1d1941b0602111bf36679b039340e1eb40
regexart <regexart@gmail.com>
2022-03-06 21:30:32 +0300

    500 kodlu hata sayfası oluşturuldu

---
+++ src/pages/500.js
@@ -0,0 +1,36 @@
+import { NextSeo } from 'next-seo'
+import { useRouter } from 'next/router'
+import { settings } from '../data/settings'
+import useTranslation from 'next-translate/useTranslation'
+import Alert from '../components/Alert'
+
+const Error500 = () => {
+  const { t } = useTranslation('errors')
+
+  const router = useRouter()
+
+  return (
+    <>
+      <NextSeo
+        title={t('error-500.title', { siteTitle: t('all:site-title') })}
+        canonical={`${settings.main.URL}${router.asPath}`}
+        openGraph={{
+          url: `${settings.main.URL}${router.asPath}`,
+          title: t('error-500.og-title', { siteTitle: t('all:site-title') }),
+          description: t('all:og-description'),
+          site_name: t('all:site-title')
+        }}
+      />
+
+      <div className="main">
+        <Alert
+          title={t('error-500.alert-title')}
+          text={t('error-500.alert-text')}
+          className={'alert-danger'}
+        />
+      </div>
+    </>
+  )
+}
+
+export default Error500
