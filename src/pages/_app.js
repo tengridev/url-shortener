facdab2f2fac3a1ffc92a3a2934995cf7fd7c6d8
Muhammet Demirel <muhammet@muhammetdemirel.com.tr>
2023-03-25 23:12:59 +0300

    Performans ve SEO iyileştirmesi yapıldı.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -81,6 +81,19 @@ function App({ Component, pageProps }) {
         ]}
         additionalLinkTags={[
           {
+            rel: 'preconnect',
+            href: 'https://fonts.googleapis.com'
+          },
+          {
+            rel: 'preconnect',
+            href: 'https://fonts.gstatic.com',
+            crossOrigin: 'anonymous'
+          },
+          {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;800&display=swap'
+          },
+          {
             rel: 'shortcut icon',
             href: `${settings.main.URL}/static/img/favicon.png`
           },
